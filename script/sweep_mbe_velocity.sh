001555088f6d9687796dfc6d97e67f87064600eb
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2026-05-28 14:17:26 +0800

    - update script

--- script/sweep_mbe_velocity.sh
+++ script/sweep_mbe_velocity.sh
@@ -52,9 +52,9 @@ MAX_TOKENS=1024
 LR=5e-6
 NUM_GEN=8
 GRAD_ACCUM=8b
-MAX_STEPS=200
+MAX_STEPS=300
 EVAL_SAMPLES=1319           # full GSM8K test set (1319 questions × NUM_GEN rollouts)
-EVAL_EVERY=50               # log eval (incl. MBE velocity) every N steps
+EVAL_EVERY=100               # log eval (incl. MBE velocity) every N steps
 
 # MBE velocity defaults
 VELO_STRIDE=8
